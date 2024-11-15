8e20b930ae0b0b5c1e759749aa779a1256fda123
SeiichiN <billie175@gmail.com>
2024-11-15 12:13:56 +0900

    rensyu55.py modified.

--- card_junbi/game2.py
+++ card_junbi/game2.py
@@ -1,5 +1,22 @@
 import random
 
+def judge(oya, user):
+    if oya[1] == 1 and user[1] == 1:
+        return 'draw'
+    elif oya[1] == 1 and user[1] != 1:
+        return 'oya'
+    elif oya[1] != 1 and user[1] == 1:
+        return 'user'
+    elif oya[1] > user[1]:
+        return 'oya'
+    elif oya[1] < user[1]:
+        return 'user'
+    else:
+        return 'draw'
+
+def print_card(oya, user):
+    print(f'親 {oya}', end=' ')
+    print(f'子 {user}')
 
 
 card_list = []
@@ -17,8 +34,24 @@ for n in range(1, 14):
     card = ("diamond", n)
     card_list.append(card)
 
-card1 = random.choice(card_list)
-print(card1)
-card_list.remove(card1)
-print(len(card_list))
+
+while True:
+    oya = random.choice(card_list)
+    card_list.remove(oya)
+
+    user = random.choice(card_list)
+    card_list.remove(user)
+
+    print(oya)
+    print('y:勝負する n:おりる q:やめる')
+    select = input('> ').strip().lower()
+    if select == 'y':
+        winner = judge(oya, user)
+        print_card(oya, user)
+        print(winner)
+    elif select == 'q':
+        break
+    else:
+        print_card(oya,user)    
+        print('チップ300円を支払った')
 
