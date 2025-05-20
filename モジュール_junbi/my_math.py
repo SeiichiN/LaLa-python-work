277858d326d557b00891b5fa92dd89f61579e6cd
SeiichiN <billie175@gmail.com>
2025-05-20 15:00:12 +0900

    my_math.py modified.

--- モジュール_junbi/my_math.py
+++ モジュール_junbi/my_math.py
@@ -11,3 +11,13 @@ def multi(x, y):
 
 def division(x, y):
     return x/y
+
+#if __name__ == '__main__':
+a = int(input('a > '))
+b = int(input('b > '))
+print(plus(a, b))
+print(minus(a, b))
+print(multi(a, b))
+print(division(a, b))
+
+
