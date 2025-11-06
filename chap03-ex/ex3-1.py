7b0999ffa799f1cd301633eb4f4d128c7d890a9d
SeiichiN <billie175@gmail.com>
2025-11-06 16:21:50 +0900

    11/6 引数まで

---
+++ chap03-ex/ex3-1.py
@@ -0,0 +1,14 @@
+# while文
+i = 10
+total = 0
+while (i <= 20):
+    total += i
+    i += 1
+print(total)
+
+# for文
+sum = 0
+for i in range(10, 21):
+    sum += i
+print(sum)
+
---
+++ chap03-ex/ex3-2.py
@@ -0,0 +1,6 @@
+sum = 0
+for i in range(10, 21):
+    if i == 15:
+        continue
+    sum += i
+print(sum)
