7aea814a09ce70dbb376bc1ddb4b83ff38304784
SeiichiN <billie175@gmail.com>
2025-11-09 21:21:13 +0900

    掛け金

--- インディアンポーカー-home/poker_money.py
+++ インディアンポーカー-home/poker_money.py
@@ -18,40 +18,56 @@ def get_com_bet(user):
     user_val = 14 if user == 1 else user
     if user_val >= 12:
         if is_fake:
-            return 10000
+            return random.choice([8000, 8500, 9000, 9500, 10000])
         return 0
     elif 8 <= user_val <= 11:
         if is_fake:
-            return 10000
-        return 5000
+            return random.choice([8000, 8500, 9000, 9500, 10000])
+        return random.choice([4000, 5000, 6000])
     elif 2 <= user_val <= 7:
         if is_fake:
-            return 3000
-        return 10000
+            return random.choice([3000, 4000, 5000])
+        return random.choice([8000, 8500, 9000, 9500, 10000])
 
-def judge(user, com):
+def judge(user, com, user_bet, com_bet):
+    rewards = CHIP*2 + user_bet + com_bet
+    global user_money
+    global com_money
     com_val = 14 if com == 1 else com
     user_val = 14 if user == 1 else user
     if user_val > com_val:
         print('あなたの勝ちです。')
+        print(f'あなたは{rewards}円を獲得しました。')
+        user_money += rewards
+        com_money -= rewards
     elif user_val < com_val:
         print('あなたの負けです。')
+        print(f'あなたは{rewards}円を失いました。')
+        user_money -= rewards
+        com_money += rewards
     else:
         print('引き分けです。')
     
 
 while True:
+    print('--- プレイ ---')
     print('チップが2,000円支払われました。')
     com = random.randint(1, 13)
     user = random.randint(1, 13)
     com_bet = get_com_bet(user)
+    com_money -= (CHIP + com_bet)
     print('相手の数字:', com)
-    user_bet = int(input('何円賭けますか? (降りる 0)'))
+    print(f'相手は{com_bet}円賭けました。')
+    print(f'あなたの所持金: {user_money}')
+    user_bet = int(input('何円賭けますか? (降りる 0) > '))
+    user_money -= (CHIP + user_bet)
     print(f'あなた:{user} わたし:{com}')
-    if user_monay > 0:
-        judge(user, com)
+    if user_bet > 0:
+        judge(user, com, user_bet, com_bet)
     else:
         print('あなたは降りました。')
+        print(f'あなたは{CHIP}円失いました。')
+        user_money -= CHIP
         
     is_end = input('終わりたければ q :')
     if is_end.lower() == 'q':
